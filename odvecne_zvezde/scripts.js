ee87e410ba81b8148bbf56c183e1bc49acba6a77
GitHuber10 <peter.andolsek@gmail.com>
2024-08-10 21:23:05 +0200

    Velik update messierja in dodan gradiva link

--- odvecne_zvezde/scripts.js
+++ odvecne_zvezde/scripts.js
@@ -1,93 +1,154 @@
-var u_neg = true;
-var u_lon;
-var u_lat;
-var u_az;
+var u_negative = true;
+// Določeno v nadaljevanju (random)
+var u_longitude;
+var u_latitude;
+var u_azimuth;
+
 var u_scale = 1.2;
-var shown = false;
 var constellations = false;
-var max_r = 8;
 var allowed_r = 6;
-var glasbene = [];
-var zvezda_arr = [];
-var nakljucneMagnitude_bool = false;
 
-/////////// INICIALIZIRAJ, ČE OBSTAJAJO GLASBENE IZ PREJŠNJEGA OBISKA
-if (sessionStorage.getItem("glasbene") == null) {
-    glasbene = [0.0, 1.5, 3.0];
-} else {
-    glasbene = JSON.parse(sessionStorage.getItem("glasbene"));
-}
-
-
-function saveGlasbene_toSesStor() {
-    sessionStorage.setItem("glasbene", JSON.stringify(glasbene));
-}
-
-function mag_to_r(mag) {
-    return Math.E ** ((1.6 - mag) / 5.0) * 3 / 1.2 * u_scale;
-}
+const pokazi_text = "Pokaži rešitev";
+const skrij_text = "Skrij rešitev";
 
 function g(id) {
     return document.getElementById(id);
 };
+
 //////// PRIDOBI OBJEKTE
 const canvas = g("canv");
 const ctx = canvas.getContext("2d");
 const iframe = g("ifr");
-const button = g("btn");
+const btn_resitev = g("btn_resitev");
 const navodilaP = g("navodilaP");
 const showConstellations_btn = g("showConstellations");
+const nakljucneMagnitude_btn = g("nakljucneMagnitude");
 var glasbeneZeljeDiv = g("glasbeneZeljeDiv");
 
+// Seznam želenih magnitud
+  // default magnitude
+glasbene = [0.0, 1.5, 3.0];
+
+var zvezda_arr = [];
+
+var number_guesses = 0;
+
+/////////// INICIALIZIRAJ, ČE OBSTAJAJO GLASBENE IZ PREJŠNJEGA OBISKA
+if (sessionStorage.getItem("glasbene") != null) {
+  data = sessionStorage.getItem("glasbene");
+  if (data[0] == "1") {
+    nakljucneMagnitude_btn.checked = true;
+    nakljucneMagnitude(nakljucneMagnitude_btn);
+    nakljucneInput.value = 3;
+
+  } else {
+    nakljucneMagnitude_btn.checked = false;
+    reloadGlasbene();
+  }
+  glasbene = JSON.parse(data.slice(1));
+}
+function saveGlasbene_toSesStor() {
+  if (nakljucneMagnitude_btn.checked) {
+      sessionStorage.setItem("glasbene", "1" + JSON.stringify(glasbene));
+  }
+  else {
+    sessionStorage.setItem("glasbene", "0" + JSON.stringify(glasbene));
+  }
+}
+
+// Preračun magnitude v radij zvezde
+function mag_to_r(mag) {
+    return Math.E ** ((1.6 - mag) / 5.0) * 3 / 1.2 * u_scale;
+}
+
 // Ob kliku: označi pravilne zvezde
-function draw(evt) {
+function checkStars(evt) {
+  if (btn_resitev.innerHTML == pokazi_text) {
     var rect = canvas.getBoundingClientRect(),
         scaleX = canvas.width / rect.width,
         scaleY = canvas.height / rect.height;
     var x = (evt.clientX - rect.left) * scaleX;
     var y = (evt.clientY - rect.top) * scaleY;
 
+    const audio_correct = new Audio("correct.wav");
+    const audio_false = new Audio("false.wav");
+    var something_found = false;
     for (const el of zvezda_arr) {
         var dist = Math.sqrt((x - el.x) ** 2 + (y - el.y) ** 2);
         if (dist <= allowed_r) {
             el.found = true;
+            audio_correct.play();
+            something_found = true;
         }
     }
-    showWithoutReload();
+    if (!something_found)
+      audio_false.play();
+    drawStars();
+
+  number_guesses++;
+
+  // preveri, če je našel vse
+  var all_found = true;
+  for (const el of zvezda_arr) {
+    if (!(el.found))
+      all_found = false;
+  }
+  if (all_found) {
+    btn_resitev.innerHTML = skrij_text;
+    btn_resitev.style.display = "none";
+    setTimeout(function(){
+      var audio_end = new Audio("end.wav");
+      audio_end.play();
+      setTimeout(function(){
+        alert("Čestitke! Našel si vse zvezde v " + number_guesses + " poskusih!");
+      }, 1500);
+
+    }, 1500);
+  }
 }
-canvas.addEventListener("click", draw);
+
+}
+canvas.addEventListener("click", checkStars);
 
 function getURL() {
-    return ("https://virtualsky.lco.global/embed/index.html?cardinalpoints=false&az=" + u_az +
-        "&longitude=" + u_lon +
-        "&latitude=" + u_lat +
+    return ("https://virtualsky.lco.global/embed/index.html?cardinalpoints=false&az=" + u_azimuth +
+        "&longitude=" + u_longitude +
+        "&latitude=" + u_latitude +
         "&gradient=false&projection=stereo&mouse=false&keyboard=false&scalestars=" + u_scale +
-        "&negative=" + u_neg +
+        "&negative=" + u_negative +
         "&constellations=" + constellations +
         "&clock=Jan%2001%202020%2001:00:00&showplanets=false&showplanetlabels=false&showdate=false&showposition=false&magnitude=6");
 }
 
-function drawStars(color="black", const_r=0) {
+function drawStar(x, y, r, color, lw=0, strokeColor="black") {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.lineWidth = lw;
+  ctx.strokeStyle = strokeColor;
+  ctx.stroke();
+}
+
+function drawStars() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (const el of zvezda_arr) {
         if (el.found) {
-            ctx.fillStyle = "#09e81c";
-        } else {
-            ctx.fillStyle = color;
+          drawStar(el.x, el.y, el.r, "#09e81c", el.r * 0.4, "black");
         }
-        ctx.beginPath();
-        if (const_r != 0) {
-          ctx.arc(el.x, el.y, const_r, 0, 2 * Math.PI);
-        } else {
-          ctx.arc(el.x, el.y, el.r, 0, 2 * Math.PI);
-        }
-        ctx.fill();
-        if (el.found) {
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "black";
-            ctx.stroke();
+        else {
+          drawStar(el.x, el.y, el.r, "black");
         }
     }
+}
+
+// dodaj solution naknadno
+function drawSolution() {
+  for (const el of zvezda_arr) {
+      if (!(el.found)) {
+        drawStar(el.x, el.y, 8, "red", 2, "black");
+      }
+  }
 }
 
 function updateNavodila(len) {
@@ -104,17 +165,20 @@ function updateNavodila(len) {
     navodilaP.innerHTML = appT + " Nebo in odvečne zvezde se ob vsaki osvežitvi strani spremenijo.";
 }
 // Dodaj popolnoma nove zvezde, osveži
-function onReload() {
-
+function restart() {
+  number_guesses = 0;
+  btn_resitev.style.display = "block";
+  btn_resitev.innerHTML = pokazi_text;
+  // generiraj zvezde
     zvezda_arr = [];
-    u_az = Math.random() * 360;
-    u_lon = Math.random() * 360 - 180;
-    u_lat = Math.random() * 180 - 90;
+    u_azimuth = Math.random() * 360;
+    u_longitude = Math.random() * 360 - 180;
+    u_latitude = Math.random() * 180 - 90;
     iframe.setAttribute("src", getURL());
 
     iframe.onload = function() {
         var len = glasbene.length;
-        if (nakljucneMagnitude_bool) {
+        if (nakljucneMagnitude_btn.checked) {
           const nakljucneInput = g("nakljucneInput");
           if (!nakljucneInput.value) {
             alert("Vnesi veljavno število odvečnih zvezd!");
@@ -123,15 +187,15 @@ function onReload() {
           }
         }
         for (var i = 0; i < len; i++) {
-            var rand1 = Math.floor(Math.random() * (canvas.width - 16) + 8);
-            var rand2 = Math.floor(Math.random() * (canvas.height - 16) + 8);
+            var rand1 = Math.random() * (canvas.width - 16) + 8;
+            var rand2 = Math.random() * (canvas.height - 16) + 8;
             var zvezda = {
                 x: rand1,
                 y: rand2,
                 r: mag_to_r(glasbene[i]),
                 found: false
             };
-            if (nakljucneMagnitude_bool) {
+            if (nakljucneMagnitude_btn.checked) {
                 const nakljucneInput = g("nakljucneInput");
                 if (nakljucneInput.value) {
                   zvezda.r = mag_to_r(Math.random() * 3.5 + 1.0);
@@ -139,47 +203,41 @@ function onReload() {
             }
             zvezda_arr.push(zvezda);
         }
-        drawStars();
         updateNavodila(len);
+        // izriši
+        drawStars();
         iframe.onload = 0;
 
         // Stalen link
 
         /*var thisUrl = window.location;
-          sentUrl = thisUrl.origin + thisUrl.pathname + "?perm=true&lat=" + u_lat +
-          "&lon=" + u_lon +
-          "&az=" + u_az +
+          sentUrl = thisUrl.origin + thisUrl.pathname + "?perm=true&lat=" + u_latitude +
+          "&lon=" + u_longitude +
+          "&az=" + u_azimuth +
           "&zvezde=" + JSON.stringify(zvezda_arr);
 
           g("permLink").href = sentUrl;
         */
-        shown = false;
-        button.innerHTML = "Pokaži rešitev";
     };
 };
 
-function show() {
-    for (const el of zvezda_arr) {
-        el.found = false;
-    }
-    if (shown) {
-        drawStars();
-        shown = false;
-        button.innerHTML = "Pokaži rešitev";
+function showSolution() {
+  drawStars();
+    if (btn_resitev.innerHTML == skrij_text) {
+        btn_resitev.innerHTML = pokazi_text;
     } else {
-        drawStars("lightgreen", 8);
-        shown = true;
-        button.innerHTML = "Skrij rešitev";
+        drawSolution();
+        btn_resitev.innerHTML = skrij_text;
     }
 };
 
-function showWithoutReload() {
+function showConstellations() {
+    constellations = showConstellations_btn.checked;
     iframe.setAttribute("src", getURL());
     iframe.onload = function() {
-        drawStars("black");
+        drawStars();
         iframe.onload = 0;
     }
-    shown = false;
 }
 
 function reloadGlasbene() {
@@ -215,10 +273,6 @@ function changeGlasbene() {
     saveGlasbene_toSesStor();
 }
 
-function showConstellations() {
-    constellations = showConstellations_btn.checked;
-    showWithoutReload();
-}
 
 function enforceMinMax(el) {
     if (el.value != "") {
@@ -233,12 +287,9 @@ function enforceMinMax(el) {
 
 function nakljucneMagnitude(e) {
     if (e.checked) {
-        nakljucneMagnitude_bool = true;
         glasbeneZeljeDiv.innerHTML = '<p>Število odvečnih zvezd: <input type="number" id="nakljucneInput" min="1" max="9" onkeyup="enforceMinMax(this)"/>' + '</p>';
     } else {
-        nakljucneMagnitude_bool = false;
         reloadGlasbene();
     }
+    saveGlasbene_toSesStor();
 }
-
-reloadGlasbene();
