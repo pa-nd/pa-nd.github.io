3d252984bc9b5397d7419cd525a4929f6c6f1b48
GitHuber10 <peter.andolsek@gmail.com>
2024-08-13 22:33:04 +0200

    posodobitve svetlih zvezd

--- svetle-zvezde/script.js
+++ svetle-zvezde/script.js
@@ -3,6 +3,10 @@ function g(id) {
 }
 var pravilni = 0;
 var napacni = 0;
+var pravilniList = [];
+var napacniList = [];
+var utezi = [];
+var randomUtezi = [];
 var najvecjiIndex;
 var currentStar;
 var planetarium;
@@ -10,15 +14,26 @@ var planetarium;
 function generirajZvezdo() {
     var randInd;
     do {
-        randInd = Math.floor(Math.random() * najvecjiIndex);
-    } while (currentStar == randInd);
+    var sum = 0;
+    for (var i = 0; i <= najvecjiIndex; i++) {
+        sum += utezi[i];
+        randomUtezi[i] = sum;
+    }
+    var randomNum = Math.random() * randomUtezi[najvecjiIndex];
+    for (var i = 0; i <= najvecjiIndex; i++) {
+        if (randomUtezi[i] >= randomNum) {
+            randInd = i;
+            break;
+        }
+    }
+} while (randInd == currentStar)
     currentStar = randInd;
+
     g("imeZvezde").innerHTML = "" + zvezdeList[currentStar].Letter + " " + zvezdeList[currentStar].Constellation + ": ";
 }
 
 
 function zacniIgro() {
-
     const mejnaMagnituda = parseFloat(g("mejnaMagnituda").value);
     g("izborDiv").style.display = "none";
     g("igraDiv").style.display = "block";
@@ -28,7 +43,12 @@ function zacniIgro() {
             break;
         }
     }
-    
+    g("naslov2").innerHTML = "Do " + mejnaMagnituda + ". magnitude (" + (najvecjiIndex + 1) + " zvezd)";
+
+    pravilniList = new Array(najvecjiIndex+1).fill(0);
+    napacniList = new Array(najvecjiIndex+1).fill(0);
+    utezi = new Array(najvecjiIndex+1).fill(1);
+    randomUtezi = new Array(najvecjiIndex+1).fill(0);
     generirajZvezdo();
 }
 function preveri() {
@@ -36,18 +56,23 @@ function preveri() {
     const text = g("igraInput").value;
     if (text.toLowerCase() == zvezda.Name.toLowerCase()) {
         pravilni++;
+        pravilniList[currentStar]++;
         g("pravilniP").innerHTML = "" + pravilni + " pravilnih";
         g("odgovor").style.color = "#79fc84";
     } else {
         napacni++;
+        napacniList[currentStar]++;
         g("napacniP").innerHTML = "" + napacni + " napačnih";
         g("odgovor").style.color = "#fc7979";
     }
+    utezi[currentStar] = 1 + 3 * (napacniList[currentStar]) / (napacniList[currentStar] + pravilniList[currentStar]);
+
     var opisText = zvezda.Letter + " " + zvezda.Constellation + ": " + zvezda.HR_classification + ", " + zvezda.Magnitude + "<sup>mag</sup>";
     g("podrobenOpis").innerHTML = opisText;
     g("odgovor").innerHTML = zvezda.Name;
     g("igraInput").value = "";
 
+    // prvic
     if (napacni + pravilni == 1) {
     planetarium = S.virtualsky({
 		'id': 'starmap1',
