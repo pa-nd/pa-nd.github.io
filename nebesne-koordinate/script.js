21d6359d75c0acc0822d04817ff9939ec1270759
pa-nd <peter.andolsek@gmail.com>
2025-08-06 12:32:24 +0200

    Dodaj interaktiven način za zvezdne koordinate in napaka v stopinjah

--- nebesne-koordinate/script.js
+++ nebesne-koordinate/script.js
@@ -43,34 +43,13 @@ g("input").innerHTML = result;*/
     return inside;
 }*/
 var planetarium;
-var RA_rand = 360 * Math.random();
-var Dec_rand = (180 * Math.random() - 90);
+var RA_rand;
+var Dec_rand;
 
-S(document).ready(function() {
-    planetarium = S.virtualsky({
-        'id': 'starmap1',
-        'projection': 'gnomic',
-        'ra': RA_rand,
-        'dec': Dec_rand,
-        'ground': false,
-        'constellations': true,
-        'fov': 35,
-        'showposition': false,
-        'showdate': false,
-        'scalestars': 2,
-        'showplanets': false,
-        'showplanetlabels': false,
-        'cardinalpoints': false
-    });
-    planetarium.addPointer({
-		'ra': RA_rand,
-		'dec': Dec_rand,
-		'label': 'lokacija',
-		'colour':'rgb(255,220,220)'
-	});
-});
 
 function potrdi() {
+    g("potrdiBtn").style.display = "none";
+    g("novoBtn").style.display = "block";
     let RA_inp = g("RA").value;
     const Dec = parseFloat(g("Dec").value);
     RA_inp = RA_inp.split(":");
@@ -109,6 +88,45 @@ function potrdi() {
 	});
     let RA_hours = Math.floor(RA_rand / 15);
     let RA_mins = Math.floor((RA_rand/15 - RA_hours)*60);
-    g("odgovor").innerHTML = "RA: " + RA_hours + "<sup>h</sup>" + RA_mins + "<sup>m</sup>, Dec: " + Math.floor(Dec_rand*100)/100 + "°";
+    Delta_RA = (RA - RA_rand)/180 * Math.PI;
+    Dec1 = Dec_rand/180 * Math.PI;
+    Dec2 = Dec/180 * Math.PI;
+    razdalja = 180/Math.PI * Math.acos(Math.sin(Dec1)*Math.sin(Dec2) + Math.cos(Dec1)*Math.cos(Dec2)*Math.cos(Delta_RA));
+    g("odgovor").innerHTML = "RA: " + RA_hours + "<sup>h</sup>" + RA_mins + "<sup>m</sup>, Dec: " + Math.floor(Dec_rand*100)/100 + "°. (Δ = " + Math.round(razdalja*10)/10 + "°)";
     planetarium.panTo(RA, Dec, 1000);
 }
+
+function novo() {
+    g("potrdiBtn").style.display = "block";
+    g("novoBtn").style.display = "none";
+    g("odgovor").innerHTML = "";
+    RA_rand = 360 * Math.random();
+    Dec_rand = (180 * Math.random() - 90);
+    
+    planetarium = S.virtualsky({
+        'id': 'starmap1',
+        'projection': 'gnomic',
+        'ra': RA_rand,
+        'dec': Dec_rand,
+        'ground': false,
+        'constellations': true,
+        'fov': 35,
+        'showposition': false,
+        'showdate': false,
+        'scalestars': 2,
+        'showplanets': false,
+        'showplanetlabels': false,
+        'cardinalpoints': false
+    });
+    planetarium.addPointer({
+		'ra': RA_rand,
+		'dec': Dec_rand,
+		'label': 'lokacija',
+		'colour':'rgb(255,220,220)'
+	});
+}
+
+
+S(document).ready(function() {
+    novo();
+});
